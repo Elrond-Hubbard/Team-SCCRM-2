7c42917d8493c77583148075bca8d21bb9c4f614
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-10-03 22:18:30 -0400

    added Patient model

---
+++ models/Patient.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Patient extends Model {}
+
+Patient.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    vitals_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "vitals",
+        key: "id",
+      },
+    },
+    doctor_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "doctor",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: "patient",
+  }
+);
+
+module.exports = Patient;
