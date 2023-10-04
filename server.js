6addf197561e141915ee9dc775ad76b091c76b3e
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-10-03 20:17:26 -0400

    added template server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: "SCCRMsecret",
+  resave: false,
+  saveUninitialized: true,
+};
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
