3810c32ebb94c82717a0d21ee176aad22d05e4b8
CambiG1123 <campbellgilliland@gmail.com>
2023-10-05 19:01:40 -0400

    sign in button routes to /home

---
+++ public/assets/script/index.js
@@ -0,0 +1,11 @@
+document.getElementById("signIn").addEventListener("click", function() {
+    
+    var physId = document.getElementById("physID").value;
+    var physPassword = document.getElementById("physPassword").value;
+
+    // // form validation 
+    // // submit data to the server
+
+    
+    window.location.href = "/home";
+});
